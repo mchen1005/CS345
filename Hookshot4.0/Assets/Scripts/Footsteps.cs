ec0bcacee6fee9117450ef6c76e8581998b7e723
wfl32 <wfl32@drexel.edu>
2020-03-23 22:45:28 -0400

    Latest Version is 4.0
    Since a lot of important files of my Hookshot3.0 went missing, I managed to recover and place everything back together. Hookshot4.0 still needs the crosshair impletemented, but everything else is fine.

---
+++ Hookshot4.0/Assets/Scripts/Footsteps.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class Footsteps : MonoBehaviour {
+
+        public float stepRate = 0.5f;
+        public float stepCoolDown;
+        public AudioClip footsteps;
+        // Use this for initialization
+        CharacterController cc;
+        void Start () {
+        cc = GetComponent<CharacterController>();
+        }
+       
+        // Update is called once per frame
+        void Update () {
+                stepCoolDown -= Time.deltaTime;
+                if (cc.isGrounded == true && (Input.GetAxis("Horizontal") != 0f || Input.GetAxis("Vertical") != 0f) && stepCoolDown < 0f){
+                        GetComponent<AudioSource>().pitch = 1f + Random.Range (-0.2f, 0.2f);
+                        GetComponent<AudioSource>().PlayOneShot (footsteps, 0.9f);
+                        stepCoolDown = stepRate;
+                }
+        }
+}
